ecc92f21e306b7963c7a024d8adf31364bf5e5f9
Daniel Ishkinin <113051490+Daniel1871@users.noreply.github.com>
2024-11-11 10:00:23 +0300

    Delete SQL/sorting and grouping/part 2 task 3.sql

--- SQL/sorting and grouping/part 2 task 3.sql
+++
@@ -1,5 +0,0 @@
-select seller_id, string_agg(category, ' - ' order by category) as category_pair
-from sellers
-where extract(year from date_reg) = 2022
-group by seller_id
-having count(distinct category) = 2 and sum(revenue) > 75000;
