748c2ba7fbc8ba746534ea7396744b177d44bd1e
Daniel Ishkinin <113051490+Daniel1871@users.noreply.github.com>
2024-11-16 10:27:22 +0300

    feat(part 1 task 1.sql): update content

--- SQL/join/part 1 task 1.sql
+++ SQL/join/part 1 task 1.sql
@@ -7,10 +7,10 @@ LIMIT 1;
 -- В задании сказано "клиента", но если, к примеру, мы хотим как-нибудь сделать клиентам, которые долго ждут, доставку побыстрее, 
 -- то какой толк от всего одного клиента, тогда хотелось бы посмотреть на всех таких клиентов:
 
---with T as (
---	select customer_id, name, (shipment_date::TIMESTAMP - order_date::TIMESTAMP) as waiting_time
---	from Orders join Customers USING(customer_id)
---)
---select *
---from T
---where waiting_time = (select max(waiting_time) from T);
+WITH T AS (
+	SELECT customer_id, name, (shipment_date::TIMESTAMP - order_date::TIMESTAMP) as waiting_time
+	FROM Orders JOIN Customers USING(customer_id)
+)
+SELECT *
+FROM T
+WHERE waiting_time = (SELECT MAX(waiting_time) FROM T);
