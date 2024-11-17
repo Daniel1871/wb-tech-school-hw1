8d6f4e6ad7af836d7bd3674252a81996536195fa
Daniel Ishkinin <113051490+Daniel1871@users.noreply.github.com>
2024-11-17 21:32:47 +0300

    style(part 3 create table.sql): delete spaces

--- SQL/window functions/part 3 create table.sql
+++ SQL/window functions/part 3 create table.sql
@@ -12,7 +12,7 @@ CREATE TABLE query (
    
 INSERT INTO query (year, month, day, userid, ts, devicetype, deviceid, query)
 VALUES
-	(2024, 11, 16, 1, 1731715200, 'android', 'dev001', 'к'),
+    (2024, 11, 16, 1, 1731715200, 'android', 'dev001', 'к'),
     (2024, 11, 16, 1, 1731715260, 'android', 'dev001', 'ку'),
     (2024, 11, 16, 1, 1731715410, 'android', 'dev001', 'к'),
     (2024, 11, 16, 1, 1731715650, 'android', 'dev001', 'купить'),
